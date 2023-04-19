cc99c0e9944751429c72ec2d48d0c00c127f6277
estieque <estiequealam6815@gmail.com>
2023-04-20 02:32:35 +0600

    webcom

--- errors/views.py
+++ errors/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound, HttpResponseServerError
 
 def custom_400_view(request, exception):
-    return HttpResponseBadRequest(render(request, '400.html'))
+    return render(request, '400.html')
 
 def custom_403_view(request, exception):
-    return HttpResponseForbidden(render(request, '403.html'))
+    return render(request, '403.html')
 
 def custom_404_view(request, exception):
-    return HttpResponseNotFound(render(request, '404.html'))
+    return render(request, '404.html')
 
 def custom_500_view(request):
-    return HttpResponseServerError(render(request, '500.html'))
+    return render(request, '500.html')
