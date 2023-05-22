bd30e7c3fec1fb3d7cb8674dee76bb202279b6e9
estieque <estiequealam6815@gmail.com>
2023-05-22 22:36:54 +0600

    webcom

--- team/models.py
+++ team/models.py
@@ -5,7 +5,7 @@ from django.utils.html import format_html
 class TeamMember(models.Model):
     member_id = models.AutoField(primary_key=True)
     member_name = models.CharField(max_length=100)
-    position = models.CharField(max_length=150)
+    position = models.CharField(blank=True, max_length=150)
     email = models.EmailField(blank=True, null=True, max_length=50)
     image = models.ImageField(blank=True, null=True, upload_to='teamimage/')
     facebook = models.URLField(blank=True, max_length=500)
