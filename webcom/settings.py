d165ffd9d60df6f71fe05ba07428783d762405fa
Naim08Ar <estiequealam6815@gmail.com>
2023-05-25 02:47:13 +0600

    webcom

--- webcom/settings.py
+++ webcom/settings.py
@@ -156,4 +156,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/workspace/static/'
