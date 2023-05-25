19b2cbf516460b4112b07623313feb786ec9beeb
Naim08Ar <estiequealam6815@gmail.com>
2023-05-25 06:50:29 +0600

    webcom

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from contact.models import ContactMessage, ContactSeoSnippets, ContactSlider
 from django.contrib import messages
-from contact.my_captcha import FormWithCaptcha
 from emaillist.models import EmailSubs
 from homepage.models import SiteSetting
 
@@ -12,8 +11,7 @@ def contact(request):
     c_seo = ContactSeoSnippets.objects.all().order_by('-s_id')[:1]
     context={'settings':settings, 
              'cslider':cslider,  
-             'c_seo':c_seo, 
-             'captcha': FormWithCaptcha}
+             'c_seo':c_seo, }
     if request.method == "GET":
         return render(request, 'contact-us.html',context)
     elif request.method=="POST":
