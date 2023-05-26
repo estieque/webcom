91759e46c6593661d62135f38fd8c510b2134fe3
Naim08Ar <estiequealam6815@gmail.com>
2023-05-27 03:34:26 +0600

    webcom

--- contact/views.py
+++ contact/views.py
@@ -1,24 +1,28 @@
-from django.shortcuts import render
+
+
+from django.shortcuts import render, redirect
 from contact.models import ContactMessage, ContactSeoSnippets, ContactSlider
-from django.contrib import messages
 from emaillist.models import EmailSubs
 from homepage.models import SiteSetting
+from django.contrib import messages
 import requests
-from django.conf import settings
-# Create your views here.
+
 def contact(request):
     settings = SiteSetting.objects.all()
     cslider = ContactSlider.objects.all().order_by('-id')[:1]
     c_seo = ContactSeoSnippets.objects.all().order_by('-s_id')[:1]
-    context={'settings':settings, 
-             'cslider':cslider,  
-             'c_seo':c_seo, }
+    context = {
+        'settings': settings,
+        'cslider': cslider,
+        'c_seo': c_seo,
+    }
+
     if request.method == "GET":
-        return render(request, 'contact-us.html',context)
-    elif request.method=="POST":
-        recaptcha_response = request.POST.get('g-recaptcha')
+        return render(request, 'contact-us.html', context)
+    elif request.method == "POST":
+        recaptcha_response = request.POST.get('g-recaptcha-response')
         data = {
-            'secret': settings.RECAPTCHA_SECRET_KEY,
+            'secret': settings[0].RECAPTCHA_SECRET_KEY,
             'response': recaptcha_response
         }
         response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
@@ -26,19 +30,24 @@ def contact(request):
 
         if result['success']:
             if 'formone' in request.POST:
-                name=request.POST.get('name')
-                email=request.POST.get('email')
-                number=request.POST.get('number')
-                location=request.POST.get('location')
-                message=request.POST.get('message')
-                en=ContactMessage(name=name,email=email,message=message,number=number,location=location)
-                en.save()
+                name = request.POST.get('name')
+                email = request.POST.get('email')
+                number = request.POST.get('number')
+                location = request.POST.get('location')
+                message = request.POST.get('message')
+                contact_message = ContactMessage(name=name, email=email, message=message, number=number, location=location)
+                contact_message.save()
                 messages.success(request, 'Votre message a été envoyé avec succès')
-            if 'formtwo' in request.POST:
-                emails=request.POST.get('emails')
-                email=EmailSubs(emails=emails)
-                email.save()
-                messages.success(request, 'Merci de nous abonner')
-        return render(request, 'contact-us.html',context)
-    
 
+            if 'formtwo' in request.POST:
+                emails = request.POST.get('emails')
+                email_subs = EmailSubs(emails=emails)
+                email_subs.save()
+                messages.success(request, 'Merci de nous abonner')
+
+            return redirect('contact')  # Replace 'contact' with your actual URL name for the contact page
+        else:
+            messages.error(request, 'La vérification reCAPTCHA a échoué. Veuillez réessayer.')
+            return redirect('contact')  # Replace 'contact' with your actual URL name for the contact page
+
+    return render(request, 'contact-us.html', context)
